6a8cd7d55412d26ae96f019b51338eb9b41ba97c
Le big Zakaria <zakghb@hotmail.com>
2025-06-12 17:56:23 +0200

    pipex

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: zel-ghab <zel-ghab@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 16:15:48 by zel-ghab          #+#    #+#             */
-/*   Updated: 2025/06/09 17:55:37 by zel-ghab         ###   ########.fr       */
+/*   Updated: 2025/06/12 17:55:59 by zel-ghab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	write_outfile(char *s)
 {
 	int	fd;
 
-	// 0644 permet de donner les perm au proprio
+	// 0644 = PERM ONLY PROPRIO
 	fd = open(s, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (fd < 0)
 		return (perror("⚠️ Write error !"), -1);
@@ -44,15 +44,21 @@ int	main(int argc, char **argv)
 		return (1);
 	else
 	{
+		// LECTURE ET ECRITURE
 		fd_infile = read_infile(argv[1]);
 		fd_outfile = write_outfile(argv[4]);
 		if (fd_infile == -1 || fd_outfile == -1)
 			return (1);
+		// VERIF DU PIPE
 		if (pipe(pipefd) == -1)
 			return (perror("⚠️ Pipe error !") ,1);
+		// CREATION DU FORK
 		pid = fork();
 		if (pid == -1)
 			return (perror("⚠️ Fork error !"), 1);
+		// PERMET DE CONNECTER MES FICHIERS A L'ENTREE OU LA SORTIE
+		dup2(fd_infile, 0);
+		dup2(pipefd[1], 1);
 	}
 	return (0);
 }
